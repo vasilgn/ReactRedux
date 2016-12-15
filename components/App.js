4b3dd32fa589dc2b21bf531ba91f554accfdc3fd
vasilgn <vasilgn86@gmail.com>
2016-12-15 15:05:23 +0200

    init

--- components/App.js
+++ components/App.js
@@ -1,5 +1,15 @@
 import React,{Component} from 'react';
 
 export default class App extends Component{
+  constructor(){
+    super();
+  }
   
+  render(){
+    return(
+      <div>
+        <h1>Hi from React!</h1>
+      </div>
+    )
+  }
 }
